aa1df7513feb8e4f769f1a05981424f07c057ccb
Adrian Kryzinski <adriankryzinski@gmail.com>
2020-09-22 19:51:08 +0200

    Add new routes for  auth/user + changes endpoints + views settings & profile

---
+++ routes/user.routes.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/logged', (req, res) => {
+    if(req.user){
+        res.render('logged', { name: req.user.displayName, photos: req.user._json.picture });
+      }
+      else{res.render('noPermission')};
+    });
+    
+// Only after logging in...
+    router.get('/profile', (req, res) => {
+      if(req.user){
+        res.render('profile');
+      }
+      else{res.render('noPermission')};
+    });
+    
+// Only after logging in...
+    router.get('/profile/settings', (req, res) => {
+      if(req.user){
+        res.render('settings');
+      }
+      else{res.render('noPermission')};
+});
+
+router.get('/no-permission', (req, res) => {
+  res.render('noPermission');
+});
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,71 +2,40 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const hbs = require('express-handlebars');
-
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const session = require('express-session');
+const passportSetup = require('./config/passport');
 
 const app = express();
 
-// configure passport provider options
-passport.use(new GoogleStrategy({
-    clientID: '393396406885-1s3f08vovfi07ftqjplddui5bfpsaqo5.apps.googleusercontent.com',
-    clientSecret: 'nktEifna3HXSh6cQ1LRbA1QO',
-    callbackURL: 'http://localhost:8000/auth/google/callback'
-}, (accessToken, refreshToken, profile, done) => {
-    done(null, profile);
-}));
-
-// serialize user when saving to session
-passport.serializeUser((user, serialize) => {
-    serialize(null, user);
-});
-
-// deserialize user when reading from session
-passport.deserializeUser((obj, deserialize) => {
-    deserialize(null, obj);
-});
-
-app.use(session({ secret: 'anything' }));
-app.use(passport.initialize());
-app.use(passport.session());
-
+// set handlebars as view engine
 app.engine('hbs', hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }));
 app.set('view engine', '.hbs');
 
+// init session mechanism
+app.use(session({ secret: 'anything' }));
+
+// init passport
+app.use(passport.initialize());
+app.use(passport.session());
+
+// standard middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['email', 'profile'] }));
-
-  app.get('/auth/google/callback', passport.authenticate('google', { failureRedirect: '/user/no-permission' }),
-  (req, res) => {
-
-    res.redirect('/user/logged');
-  } 
-);
-
-
 app.get('/', (req, res) => {
-    res.render('index');
+  res.render('index');
 });
 
-app.get('/user/logged', (req, res) => {
-    res.render('logged');
-});
-
-app.get('/user/no-permission', (req, res) => {
-    res.render('noPermission');
-});
+app.use('/auth', require('./routes/auth.routes'));
+app.use('/user', require('./routes/user.routes'));
 
 app.use('/', (req, res) => {
-    res.status(404).render('notFound');
+  res.status(404).render('notFound');
 });
 
 app.listen('8000', () => {
-    console.log('Server is running on port: 8000');
+  console.log('Server is running on port: 8000');
 });
